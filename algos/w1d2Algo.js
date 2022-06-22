5c225cf69428a82cc42a7ed4c21861d6694b6730
EricD1996 <EricD8816051@gmail.com>
2022-06-22 09:28:54 -0700

    algos intro

---
+++ algos/w1d2Algo.js
@@ -0,0 +1,20 @@
+var name = "Eric";
+var age = 29;
+var ch1pPrice = 9.75;
+var woke = true;
+var isTeachingToday = true;
+console.log("What's up"+name)
+ch1pPrice += 1;
+console.log(ch1pPrice)
+
+var halfMyAge = age/2;
+var twiceMyAge = age*2;
+
+console.log(halfMyAge)
+console.log(twiceMyAge)
+
+var artists = ["Drake", "Weezy", "Weekend", "NIN", "Saint John", "Taylor Swift"];
+
+for(var x = 0; x < artists.length; x++){
+    console.log(artists[x]);
+}
